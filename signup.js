4b30d74e5472a59d815033b9318f10bcb6b38444
Harneet Singh <62827797+harneet2512@users.noreply.github.com>
2024-01-16 18:10:50 +0530

    Delete signup.js

--- signup.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById("toggle");
-const Changer = document.querySelectorAll(".changer");
-
-toggle.addEventListener("click", () => {
-  Changer.forEach((el) => {
-    el.classList.toggle("change");
-  });
-});
